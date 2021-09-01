7ac6d2f3c2d74855ac7be0d0c4b1ad3aaa13b39f
priyohaque <86319438+priyohaque@users.noreply.github.com>
2021-09-01 22:04:43 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+            <!-- Copyright info -->
+            <p class="copy center">Copyright &copy; 2021 |
+             <a href="#">Online KUSUMNIBASH Restaurant Management System</a> </p>
+      </div>
+    </div>
+  </div>
+</footer> 
